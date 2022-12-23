7b5b981e954eeda3eae2b9da20607ff16692976c
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-23 15:10:15 -0300

    feat(home): create layout claim username form component

--- src/pages/home/ClaimUsernameForm/index.tsx
+++ src/pages/home/ClaimUsernameForm/index.tsx
@@ -1,15 +1,53 @@
-import { TextInput, Button } from '@ignite-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form } from './styles'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O nome de usuário deve ter no mínimo 3 caracteres.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O nome de usuário deve conter apenas letras e hífens.',
+    })
+    .transform((rawUsername) => rawUsername.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handlePreRegister(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu usuário" />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handlePreRegister)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username?.message || 'Digite o nome do usuário desejado.'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
--- src/pages/home/ClaimUsernameForm/styles.ts
+++ src/pages/home/ClaimUsernameForm/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, Text, styled } from '@ignite-ui/react'
 
 export const Form = styled(Box, {
   display: 'grid',
@@ -9,5 +9,12 @@ export const Form = styled(Box, {
 
   '@media(max-width: 600px)': {
     gridTemplateColumns: '1fr',
+  },
+})
+
+export const FormAnnotation = styled('div', {
+  [`> ${Text}`]: {
+    marginTop: '$2',
+    color: '$gray400',
   },
 })
