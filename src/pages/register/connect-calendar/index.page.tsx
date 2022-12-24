492ef60d126d21e503356f58105618ba6a1ace52
Vitor Emídio <vitorsemidio.96@gmail.com>
2022-12-23 22:20:38 -0300

    feat(register): add oAuth google

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -2,6 +2,7 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { Container, Header } from '../styles'
 import { ConnectItem, ConnectBox } from './styles'
+import { signIn } from 'next-auth/react'
 
 export default function Register() {
   return (
@@ -17,7 +18,11 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar <ArrowRight />
           </Button>
         </ConnectItem>
