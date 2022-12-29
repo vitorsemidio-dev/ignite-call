9a3e560485daf27162cd79dcbd3712b7a1ff6190
Vitor Emídio <vitorsemidio.96@gmail.com>
2022-12-28 21:56:16 -0300

    feat: create page UserSchedule only with user details

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,59 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { convertDaysToSeconds } from '../../../utils/convert'
+import { Container, UserHeader } from './styles'
+
+interface UserScheduleProps {
+  user: {
+    name: string
+    username: string
+    avatarUrl: string
+  }
+}
+
+export default function UserSchedule({ user }: UserScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.username}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const username = String(ctx.params?.username)
+
+  const prismaUser = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!prismaUser) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: prismaUser.name,
+        username: prismaUser.username,
+        avatarUrl: prismaUser.avatar_url,
+      },
+    },
+    revalidate: convertDaysToSeconds(1),
+  }
+}
